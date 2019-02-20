d797a43cdbb980824a495a3666a36841edbcdce8
Alex Young <alex.young@hive.hr>
2019-02-20 21:47:32 +0000

    Add missing export of http verbs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./core/app";
+export { HTTP_METHOD } from "./core/httpMethod";
 export { Injection } from "./core/types";
 export { RestResult } from "./core/RestResult";
 export { createMockLogger } from "./mocks/logger";
